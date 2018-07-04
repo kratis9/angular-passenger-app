f497f52dd8c57039b7bd66952983e9719ed9aee4
krati <kratis9@gmail.com>
2018-07-04 19:46:27 +1000

    add logic for passenger dashboard

--- src/app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
+++ src/app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
@@ -1,16 +1,22 @@
-import { Component, Input } from '@angular/core';
-import { Passenger } from '../../models/passenger';
-
+import { Component, Input, Output } from "@angular/core";
+import { Passenger } from "../../models/passenger";
 
 @Component({
-  selector: 'passenger-detail',
-  templateUrl: 'passenger-detail.component.html',
-  styleUrls: ['passenger-detail.component.css']
+  selector: "passenger-detail",
+  templateUrl: "passenger-detail.component.html",
+  styleUrls: ["passenger-detail.component.css"]
 })
 export class PassengerDetailComponent {
-  @Input()
-  detail:Passenger;
-  onNameChange(value: string){
-console.log('value:', value);
+  @Input() detail: Passenger;
+  editing: boolean = false;
+
+  onNameChange(value: string) {
+    this.detail.fullname = value;
+  }
+  toggleEdit() {
+    this.editing = !this.editing;
+  }
+  onRemove() {
+    this.editing = !this.editing;
   }
 }
--- src/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ src/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -1,46 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-import { Passenger } from '../../models/passenger';
+import { Passenger } from "../../models/passenger";
 
 @Component({
-  selector: 'passenger-dashboard',
-  styleUrls: ['passenger-dashboard.component.css'],
-  templateUrl: 'passenger-dashboard.component.html'
+  selector: "passenger-dashboard",
+  styleUrls: ["passenger-dashboard.component.css"],
+  templateUrl: "passenger-dashboard.component.html"
 })
 export class PassengerDashboardComponent implements OnInit {
   passengers: Passenger[];
-  constructor() { }
+  constructor() {}
   ngOnInit() {
-    this.passengers = [{
-      id: 1,
-      fullname: 'Stephen',
-      checkedIn: true,
-      checkInDate: 1490742000000,
-      children: null
-    }, {
-      id: 2,
-      fullname: 'Rose',
-      checkedIn: false,
-      checkInDate: null,
-      children: [{ name: 'Ted', age: 12 }, { name: 'Chloe', age: 7 }]
-    }, {
-      id: 3,
-      fullname: 'James',
-      checkedIn: true,
-      checkInDate: 1491606000000,
-      children: null
-    }, {
-      id: 4,
-      fullname: 'Louise',
-      checkedIn: true,
-      checkInDate: 1488412800000,
-      children: [{ name: 'Jessica', age: 1 }]
-    }, {
-      id: 5,
-      fullname: 'Tina',
-      checkedIn: false,
-      checkInDate: null,
-      children: null
-    }];
+    this.passengers = [
+      {
+        id: 1,
+        fullname: "Stephen",
+        checkedIn: true,
+        checkInDate: 1490742000000,
+        children: null
+      },
+      {
+        id: 2,
+        fullname: "Rose",
+        checkedIn: false,
+        checkInDate: null,
+        children: [{ name: "Ted", age: 12 }, { name: "Chloe", age: 7 }]
+      },
+      {
+        id: 3,
+        fullname: "James",
+        checkedIn: true,
+        checkInDate: 1491606000000,
+        children: null
+      },
+      {
+        id: 4,
+        fullname: "Louise",
+        checkedIn: true,
+        checkInDate: 1488412800000,
+        children: [{ name: "Jessica", age: 1 }]
+      },
+      {
+        id: 5,
+        fullname: "Tina",
+        checkedIn: false,
+        checkInDate: null,
+        children: null
+      }
+    ];
+  }
+
+  handleRemove(event) {
+    console.log(event);
   }
 }
